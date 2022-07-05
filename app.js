d9e7ba34d1c6c540478d517dbf8a2afadd0ed14b
AdilsonBND <adilsonbnd@hotmail.com>
2022-07-05 05:41:41 -0300

    Ajuste na conexão mongoose

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const { default: mongoose } = require('mongoose');
-require('dotenv').config()
 const app = express()
 
 
@@ -15,14 +14,10 @@ const personRoutes = require('./routes/PersonRoutes')
 
 app.use('/person', personRoutes)
 
-//const DB_USER = 'AdilsonBND'
-//const DB_PASSWORD = encodeURIComponent('Bianca1301@@@')
-
 
 mongoose
 .connect(
-    'mongodb://root:root@localhost:27017/?authSource=admin'
-    //`mongodb+srv://${DB_USER}:${DB_PASSWORD}@apicluster.foec6.mongodb.net/?retryWrites=true&w=majority`
+    'mongodb://root:root@mongodb:27017/?authSource=admin'
     )
 .then(() => {
     console.log('conectado ao mongo ')
