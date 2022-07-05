edb8c58794776c26c7288f1447ce3badd2389723
AdilsonBND <adilsonbnd@hotmail.com>
2022-07-05 05:44:46 -0300

    uograde

--- app.js
+++ app.js
@@ -14,20 +14,10 @@ const personRoutes = require('./routes/PersonRoutes')
 
 app.use('/person', personRoutes)
 
-<<<<<<< HEAD
 
 mongoose
 .connect(
     'mongodb://root:root@mongodb:27017/?authSource=admin'
-=======
-
-
-
-mongoose
-.connect(
-    'mongodb://root:root@localhost:27017/?authSource=admin'
-  
->>>>>>> df436cec2d9841308321d29347f171f1146fa6de
     )
 .then(() => {
     console.log('conectado ao mongo ')
